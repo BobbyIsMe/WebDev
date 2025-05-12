4297f1063535e4a174330004f2da34eb1be7bdf5
Vyirps <11820072@usc.edu.ph>
2025-05-12 21:48:28 +0800

    fix pathing on css in perosnal

--- pages/Profilepage/PersonalDetails.php
+++ pages/Profilepage/PersonalDetails.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/Webpage/landingpage.css">
-    <link rel="stylesheet" href="../css/navbar.css" />
+    <link rel="stylesheet" href="../../css/navbar.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="../js/auth.js"></script>
     <script type="text/javascript" src="../js/personal_details.js"></script>
