b85f1aa2dd2bd0af48dd765187dd5f4eaa27cc58
BobbyIsMe <joshuacc823@gmail.com>
2025-05-16 03:52:31 +0800

    add favicon

--- pages/Webpages/ContactUs.php
+++ pages/Webpages/ContactUs.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Matias BH | ContactUs</title>
+    <link rel="icon" href="../../img/favicon.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../css/Webpage/ContactUs.css">
     <link rel="stylesheet" href="../../css/navbar.css">
